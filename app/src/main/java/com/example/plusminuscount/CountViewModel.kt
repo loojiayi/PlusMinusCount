594202765619f43e43440428025583dd77fe5dc2
loojiayi <loojy-wm17@student.tarc.edu.my>
2019-12-12 15:23:05 +0800

    Done

---
+++ app/src/main/java/com/example/plusminuscount/CountViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.plusminuscount
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel(){
+
+    private val _count = MutableLiveData<Int>()         //only can access using getter n setter
+
+    val count: LiveData<Int>
+        get() = _count
+
+    init {
+        _count.value = 0
+        Log.d("ViewModel", "ViewModel initialized")
+    }
+
+    fun increment(){
+        _count.value = _count.value?.plus(1)
+    }
+
+    fun decrement(){
+        _count.value = _count.value?.minus(1)
+    }
+
+    override fun onCleared() {
+        Log.d("ViewModel", "ViewModel cleared")
+        super.onCleared()
+    }
+}
